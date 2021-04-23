adf98d36668dcee0061bc22b2c15a0c85d3bca38
Mona Lisa <sam.roberson32@gmail.com>
2021-04-22 21:10:36 -0600

    added arrays, linked css and js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
+//arrays for computer random options
+
+var letters ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ;]
+
+var numbers [0,1,2,3,4,5,6,7,8,9 ;]
+
+var specChar ["!", "@", "$";]
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
